a61e2e73cf4fdb06abff338606429c6034e888e4
maiyq1 <mijael.yq2002@outlook.com>
2024-06-11 19:36:15 -0500

    add BottomNavigationView

--- app/src/main/java/com/example/upc_carga_facil/MainActivity.kt
+++ app/src/main/java/com/example/upc_carga_facil/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.upc_carga_facil.Fragmentos.FragmentInicio
 import com.example.upc_carga_facil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,62 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        verFragmentInicio()
+
+        binding.BottomNV.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.Item_Inicio->{
+                    verFragmentInicio()
+                    true
+                }
+                R.id.Item_Chats->{
+                    verFragmentChats()
+                    true
+                }
+                R.id.Item_Servicios->{
+                    verFragmentServicios()
+                    true
+                }
+                R.id.Item_Cuenta->{
+                    verFragmentCuenta()
+                    true
+                } else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun verFragmentInicio() {
+        binding.TituloRl.text = "Inicio"
+        val fragment = FragmentInicio()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentInicio")
+        fragmentTransition.commit()
+    }
+
+    private fun verFragmentChats() {
+        binding.TituloRl.text = "Chats"
+        val fragment = FragmentInicio()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentChats")
+        fragmentTransition.commit()
+    }
+
+    private fun verFragmentServicios() {
+        binding.TituloRl.text = "Servicios"
+        val fragment = FragmentInicio()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentServicios")
+        fragmentTransition.commit()
+    }
+
+    private fun verFragmentCuenta() {
+        binding.TituloRl.text = "Cuenta"
+        val fragment = FragmentInicio()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentCuenta")
+        fragmentTransition.commit()
     }
 }

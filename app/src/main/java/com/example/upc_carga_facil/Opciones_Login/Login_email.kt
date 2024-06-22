c3b3e0bb7dce37c4ce5b6e8c552afb279b6c0c98
maiyq1 <mijael.yq2002@outlook.com>
2024-06-22 11:02:06 -0500

    activityRegistrarUsuario

--- app/src/main/java/com/example/upc_carga_facil/Opciones_Login/Login_email.kt
+++ app/src/main/java/com/example/upc_carga_facil/Opciones_Login/Login_email.kt
@@ -1,21 +1,25 @@
 package com.example.upc_carga_facil.Opciones_Login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.upc_carga_facil.R
+import com.example.upc_carga_facil.Registro_email
+import com.example.upc_carga_facil.databinding.ActivityLoginEmailBinding
 
 class Login_email : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginEmailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.TxtRegistrarme.setOnClickListener {
+            startActivity(Intent(this@Login_email, Registro_email::class.java))
         }
     }
 }
